190e91e706e920ab4730b3e8c30ae05f77ec97ad
scfi-odoo <scfi@odoo.com>
2025-11-03 08:39:30 -0500

    gettin there.... i have a menu, a pre-build of a BST, and the ability to print in-order

--- CH11BST/BinaryTree/Main.java
+++ CH11BST/BinaryTree/Main.java
@@ -18,6 +18,11 @@ public class Main {
     // root node for the binary tree
     Node root;
 
+    // sets root equal to null, clearing the tree
+    public void clear() {
+        root = null;
+    }
+
     // Constructor method
     public Main() {
         root = null;
@@ -42,7 +47,7 @@ public class Main {
         return node;
     }
 
-    // Find method asking for the node to find
+    // Find method asking for the node to find a search key
     public Node find(int key) {
         Node node = findNode(root, key);
         return node;
@@ -76,24 +81,70 @@ public class Main {
         return node;
     }
 
+    // builds balanced preset BST
+    public void buildPreset() {
+        clear();
+        int[] data = {4, 2, 6, 1, 3, 5, 7};
+        for (int i = 0; i < data.length; i++)
+            insert(i);
+    }
+
+    // prints an in-order traversal of the BST
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    public void printInOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        // traverse left subtree
+        printInOrder(node.leftChild);
+        // visit node
+        System.out.print(node.data + " ");
+        // traverse right subtree
+        printInOrder(node.rightChild);
+    }
+
+    // method to print the menu options for the user interface
+    private static void printMenu() {
+        System.out.println("\n===== BST MENU =====");
+        System.out.println("1) Create a binary search tree");
+        System.out.println("2) Add a node");
+        System.out.println("3) Delete a node");
+        System.out.println("4) Print nodes by InOrder");
+        System.out.println("5) Print nodes by PreOrder");
+        System.out.println("6) Print nodes by PostOrder");
+        System.out.println("7) Exit program");
+        System.out.println("====================");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Main binaryTree = new Main();
-        // Asks the user to input 10 integers, and then inserts each user input into the binary tree
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Please enter value " + (i + 1) + " for the binary tree: ");
-            binaryTree.insert(scanner.nextInt());
-        }
+        // program loops while running = true
+        boolean running = true;
 
-        // Collect user input to search for a node
-        System.out.println("Please enter the node to search for: ");
-        Node node = binaryTree.find(scanner.nextInt());
-        // if node not found using .find()
-        if (node == null) {
-            System.out.println("The given number does not exist in the dataset.");
-        } else { // if node found using .find()
-            System.out.println("Node " + node.data + " was found in the dataset.");
+        while (running) {
+            printMenu(); // show menu options 1-7
+            int userInput = scanner.nextInt();
+
+            if (userInput == 1) {
+                binaryTree.buildPreset();
+            }
+
+            else if (userInput == 4) {
+                binaryTree.printInOrder();
+            }
+
+            else if (userInput == 7) {
+                System.out.println("Exiting program...");
+                running = false;
+            }
         }
         scanner.close();
     }
 }
+
+// source:
+// https://www.geeksforgeeks.org/dsa/binary-search-tree-traversal-inorder-preorder-post-order/
